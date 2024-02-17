87cfd006baad43767e44d499a2223c31cdbf59c3
Jakir Hossain <js.hossain256@gmail.com>
2024-02-17 13:49:54 -0500

    Added auth redirection logic

--- src/Layout/PageLayout/PageLayout.jsx
+++ src/Layout/PageLayout/PageLayout.jsx
@@ -1,26 +1,53 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import React from "react";
 import { useLocation } from "react-router-dom";
 import SideBar from "../../components/SideBar/SideBar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
+import Navbar from "../../components/NavBar/NavBar";
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
-  const [user] = useAuthState(auth);
+  const [user, logInLoading] = useAuthState(auth);
   const canRenderSideBar = pathname !== "/auth" && user;
+  const canRenderLoginNav = !user && !logInLoading && pathname !== "/auth";
+
+  const userIsAuthenticating = !user && logInLoading;
+
+  if (userIsAuthenticating) return <PageLayoutSpinner />;
+
   return (
-    <Flex>
+    <Flex flexDir={canRenderLoginNav ? "column" : "row"} className="PAGELAYOUT">
       {/* side bar on the left */}
       {canRenderSideBar && (
         <Box w={{ base: "70px", md: "240px" }}>
           <SideBar />
         </Box>
       )}
+      {/* navbar on top  */}
+      {canRenderLoginNav && <Navbar />}
+
       {/* content on the right */}
-      <Box flex={1} w={{ base: "calc(100% - 70px", md: "calc(100% - 240px" }}>
+      <Box
+        flex={1}
+        w={{ base: "calc(100% - 70px", md: "calc(100% - 240px" }}
+        mx={"auto"}
+      >
         {children}
       </Box>
+    </Flex>
+  );
+};
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex
+      flexDir={"column"}
+      h={"100vh"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Spinner size={"xl"} />
     </Flex>
   );
 };
--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,4 +1,11 @@
-import { setDoc, doc } from "firebase/firestore";
+import {
+  setDoc,
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { auth, firestore } from "../firebase/firebase";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import useShowToast from "./useShowToast";
@@ -20,6 +27,15 @@ const useSignUpWithEmailAndPassword = () => {
       !inputs.username
     ) {
       showToast("Error", "Please fill out all fields", "error");
+      return;
+    }
+
+    const usersRef = collection(firestore, "users");
+    const q = query(usersRef, where("username", "==", inputs.username));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      showToast("Error", "Username already exists!", "error");
       return;
     }
 
